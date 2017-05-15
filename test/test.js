b40bba194a2808ec2bb101c62246bd056a68a48d
mhelvens <mhelvens@gmail.com>
2017-05-15 16:30:19 +0100

    second attempt

--- test/test.js
+++ test/test.js
@@ -1,41 +1,10 @@
 import * as babel from 'babel-core';
-
 import generate from 'babel-generator';
+import fs from 'fs';
 
 import plugin from '../src/index.js';
 
-const code = `
-	/**
-	 * Wrapper function...
-	 * @wrapper
-	 */
-	export default () => {
-		
-		/**
-		 * Class C.
-		 * @public
-		 */
-		return class C {}
-		
-		// return C;
-		
-	};
-	
-	/**
-	 * Non-wrapper function...
-	 */
-	let x = () => {
-		
-		/**
-		 * Class D.
-		 * @public
-		 */
-		class D {}
-		
-		return D;
-		
-	};
-`;
+const code = fs.readFileSync('./test/testfile.js');
 
 const newAST = babel.transform(code, {
 	plugins: [plugin]
--- test/testfile.js
+++ test/testfile.js
@@ -1,15 +1,31 @@
+import {defaults, isInteger, isObject, entries, assign} from 'lodash-bound';
+import assert from 'power-assert';
+
+import {humanMsg, definePropertiesByValue, babelHelpers, ValueTracker} from 'utilities';
+
+import {constraint} from './util/misc';
+
+const $$allowInvokingConstructor = Symbol('$$allowInvokingConstructor');
+
+////////////////////////////////////////////////////////////////////////////////
+
 /**
- * Test comment.
- * @unwrap
+ * @wrapper
  */
-export default () => {
-	
+export default (env) => {
 	/**
-	 * Class C.
+	 * The base-class of all entities described in the manifest.
 	 * @public
 	 */
-	class C {}
+	class Entity extends ValueTracker {
+		
+		static get environment() { return env }
+		
+		static get Field() { return this.environment.Field }
+		
+		static get Entity() { return Entity }
+		
+	}
 	
-	return C;
-	
+	return env.Entity || Entity;
 };
